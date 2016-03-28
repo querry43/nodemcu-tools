241d1e757d35d84c20f0af888e5c0075c7aa5983
Matt Harrington <querry43@gmail.com>
2016-03-27 22:23:58 -0700

    inline everyting

--- webserver.lua
+++ webserver.lua
@@ -1,24 +1,29 @@
+request_lc = dofile('webserver/request.lc')
+exists_lc = dofile('file/exists.lc')
+serve_file_lc = dofile('webserver/serve-file.lc')
+header_lc = dofile('webserver/header.lc')
+
 return function ()
   local function process_request(s, request)
     print('BEGIN:process_request:' .. tmr.now())
-    local request = dofile('webserver/request.lc')(request)
+    local request = request_lc(request)
     print('got request:' .. tmr.now())
 
     if request['path'] then
       print('got path:' .. tmr.now())
-      if dofile('file/exists.lc')('htdocs' .. request['path']) then
+      if exists_lc('htdocs' .. request['path']) then
         print('is static file:' .. tmr.now())
-        dofile('webserver/serve-file.lc')(s, 'htdocs' .. request['path'], false)
-      elseif dofile('file/exists.lc')('htdocs' .. request['path'] .. '.gz') then
-        dofile('webserver/serve-file.lc')(s, 'htdocs' .. request['path'] .. '.gz', true)
-      elseif dofile('file/exists.lc')('htdocs' .. request['path'] .. '.lc') then
+        serve_file_lc(s, 'htdocs' .. request['path'], false)
+      elseif exists_lc('htdocs' .. request['path'] .. '.gz') then
+        serve_file_lc(s, 'htdocs' .. request['path'] .. '.gz', true)
+      elseif exists_lc('htdocs' .. request['path'] .. '.lc') then
         local status, response = dofile('htdocs' .. request['path'] .. '.lc')(request['path'], request['query'])
-        s:send(dofile('webserver/header.lc')(status, false) .. response)
+        s:send(header_lc(status, false) .. response)
       else
-        s:send(dofile('webserver/header.lc')(404, false))
+        s:send(header_lc(404, false))
       end
     else
-      s:send(dofile('webserver/header.lc')(400, false))
+      s:send(header_lc(400, false))
     end
 
     print('closing:' .. tmr.now())
--- webserver/serve-file.lua
+++ webserver/serve-file.lua
@@ -3,6 +3,6 @@ return function (s, f, gz)
   file.open(f)
   local contents = file.read(1460)
   file.close()
-  s:send(dofile('webserver/header.lc')(200, gz) .. contents)
+  s:send(header_lc(200, gz) .. contents)
   print('END:serve-file.lc:' .. tmr.now())
 end
